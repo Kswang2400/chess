9b5a441041b428c42eac56da50642edfbe870fe5
App Academy Student <student@appacademy.io>
2015-04-29 18:05:01 -0700

    Mid of working on en passant, error

--- piece.rb
+++ piece.rb
@@ -38,6 +38,11 @@ class Piece
     board.update_square(pos, end_pos)
     self.pos = end_pos
     self.has_moved = true
+
+    #remove en passant possibilities
+    (board.all_pieces(:B) + board.all_pieces(:W))
+      .select { |piece| piece.class == Pawn }
+      .each   { |pawn|  pawn.just_moved_two = false }
   end
 
   def piece_moves
@@ -202,8 +207,31 @@ class Bishop < SlidingPiece
 end
 
 class Pawn < Piece
+  attr_accessor :just_moved_two
+
+  def initialize(pos, board, color, has_moved = false)
+    super
+    just_moved_two = false
+  end
+
   def display
     "♟"
+  end
+
+  def valid_moves
+    reg_moves = super
+
+    if pos[0] == (color == :W ? 3 : 4)
+      adjacent_pcs = [ @board.square([pos[0], pos[1] + 1]),
+                       @board.square([pos[0], pos[1] - 1]) ]
+      adjacent_pcs.each do |piece|
+        if piece && piece.class == Pawn && piece.just_moved_two
+          reg_moves << [pos[0] + color == :W  ? (-1) : (1), piece.pos[1]]
+        end
+      end
+    end
+
+    reg_moves
   end
 
   def piece_moves
@@ -229,12 +257,14 @@ class Pawn < Piece
   end
 
   def make_move!(end_pos)
+    if pos[2]
     if end_pos[0] == (color == :B ? 7 : 0)
       @board.set_square(end_pos, Queen.new(end_pos, @board, color, true))
       @board.set_square(pos, nil)
     else
       super
     end
+    just_moved_two = true if (pos[1] - end_pos[1]).abs == 2
   end
 end
 
